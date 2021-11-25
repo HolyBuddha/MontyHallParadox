f7ae8603f243661c9c2be026e8a1fbf2dc81db25
Vladimir Izmaylov <holybuddha@yandex.ru>
2021-11-25 23:40:41 +0300

    Initial Commit

---
+++ Monty Hall Paradox/Monty_Hall_ParadoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Monty_Hall_ParadoxApp.swift
+//  Monty Hall Paradox
+//
+//  Created by Vladimir Izmaylov on 25.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct Monty_Hall_ParadoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
