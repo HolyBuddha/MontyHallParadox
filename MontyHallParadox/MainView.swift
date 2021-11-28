3889d9936aceb197c6bb2c86032240b2dc6f6747
Vladimir Izmaylov <holybuddha@yandex.ru>
2021-11-29 00:59:44 +0300

    Added corners and OK Button

--- MontyHallParadox/MainView.swift
+++ MontyHallParadox/MainView.swift
@@ -7,9 +7,11 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @State private var userTapButton = false
-    @State private var buttonScore = 0
+struct MainView: View {
+    // Настрой переход с экрана на экран
+    @State private var doorTapButton = false
+    @State private var okTapButton = false
+    @State private var okButtonScore = 0
     
     @State private var strokeColor1: Color = .white
     @State private var strokeColor2: Color = .white
@@ -17,9 +19,13 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            Color.blue
+            LinearGradient(
+                gradient: Gradient(colors: [.green, .blue]),
+                startPoint: UnitPoint(x: 0, y: -1),
+                endPoint: UnitPoint(x: -1, y: 0)
+            )
                 .ignoresSafeArea()
-                .opacity(0.7)
+                .opacity(0.5)
             VStack(spacing: 30){
                 Text(changeMainText())
                     .font(.title)
@@ -29,41 +35,55 @@ struct ContentView: View {
                     .frame(width: 250, height: 100)
                 HStack(spacing: 30){
                     ButtonLabel(color: .orange, action: {
-                        buttonDoorTapped();
+                        doorButtonTapped();
                         changeStrokeColor1()
-                    }, strokeColor: strokeColor1)
+                    },
+                                strokeColor: strokeColor1)
                     
                     ButtonLabel(color: .orange, action: {
-                        buttonDoorTapped();
+                        doorButtonTapped();
                         changeStrokeColor2()
-                    }, strokeColor: strokeColor2)
+                    },
+                                strokeColor: strokeColor2)
                 }
-                ButtonLabel(color: .orange
-                            , action: {
-                    buttonDoorTapped();
+                
+                ButtonLabel(color: .orange, action: {
+                    doorButtonTapped();
                     changeStrokeColor3()
-                }, strokeColor: strokeColor3)
-                Button("OK", action: {})
+                },
+                            strokeColor: strokeColor3)
+                
+                Button("OK", action: {okButtonTapped()})
                     .frame(width: 80, height: 40)
-                    .background(userTapButton ? .green : .white)
                     .cornerRadius(20)
-                    .disabled(!userTapButton)
-                    .opacity(userTapButton ? 1 : 0)
+                    .background(.green)
+                    .disabled(!doorTapButton)
                     .foregroundColor(.white)
-                    //.padding()
+                    .overlay(
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(Color.white, lineWidth: 5)
+                        )
+                    .cornerRadius(20)
+                    .opacity(doorTapButton ? 1 : 0)
+                    
+                .padding()
                 Spacer()
-                }
-            
-        
             }
-      
+            
+            
         }
+        
+    }
 }
 
-extension ContentView {
-    private func buttonDoorTapped() {
-        //buttonScore += 1
-        userTapButton = true
+extension MainView {
+    private func doorButtonTapped() {
+        doorTapButton = true
+    }
+    
+    private func okButtonTapped() {
+        okButtonScore += 1
+        if okButtonScore == 2 { okTapButton }
     }
     private func changeStrokeColor1() {
         strokeColor1 = .red
@@ -83,7 +103,7 @@ extension ContentView {
     
     
     private func changeMainText() -> String {
-        switch buttonScore {
+        switch okButtonScore {
         case 1: return "Do you want to change your door?"
         default:
             return "Choose your door"
@@ -92,6 +112,6 @@ extension ContentView {
 }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MainView()
     }
 }
--- MontyHallParadox/MontyHallParadoxApp.swift
+++ MontyHallParadox/MontyHallParadoxApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MontyHallParadoxApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
     }
 }
